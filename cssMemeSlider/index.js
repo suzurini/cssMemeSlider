da9f7ec4026e3f2dddfa2d3df8eb96862ac35ec4
suzurini <valloforr@gmail.com>
2023-09-17 17:23:01 +0300

    feat: add const for controls and slides index.js (SUN, SEP 17, 2023 11:45:05 PM)

---
+++ cssMemeSlider/index.js
@@ -0,0 +1,15 @@
+const BTN_FIRST = document.querySelector('#control-1');
+const BTN_SECOND = document.querySelector('#control-2');
+const BTN_THIRD = document.querySelector('#control-3');
+const BTN_FOURTH = document.querySelector('#control-4');
+const BTN_FIFTH = document.querySelector('#control-5');
+const BTN_SIXTH = document.querySelector('#control-6');
+const BTN_SEVENTH = document.querySelector('#control-7');
+
+const fisdtSlide = document.querySelector('#slide-1');
+const secondSlide = document.querySelector('#slide-2');
+const thirdSlide = document.querySelector('#slide-3');
+const fourthSlide = document.querySelector('#slide-4');
+const fifthSlide = document.querySelector('#slide-5');
+const sixthSlide = document.querySelector('#slide-6');
+const seventhSlide = document.querySelector('#slide-7');
